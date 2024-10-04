8fdbf358213bfce6daa018c4cb6dd76ef33adc8f
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-10-04 16:50:24 -0300

    Criação da classe GameMenager com padrão Singleton para gerenciar o jogo.

---
+++ AVingançaDeBlaid-2D/Assets/Scripts/GameMenager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenager : MonoBehaviour
+{
+    public static GameMenager Instance;
+    public InputMenager inputMenager { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+            Destroy(this.gameObject);
+        Instance = this;
+        inputMenager = new InputMenager();
+    }
+
+}
--- AVingançaDeBlaid-2D/Assets/Scripts/Player/PlayerBehavior.cs
+++ AVingançaDeBlaid-2D/Assets/Scripts/Player/PlayerBehavior.cs
@@ -5,16 +5,10 @@ public class PlayerBehavior : MonoBehaviour
     [SerializeField]
     private float movieSpead = 10;
 
-    private InputMenager inputMenager;
-    void Start()
-    {
-        inputMenager = new InputMenager();
-    }
-
     private void Update()
     {
-        float movieDirection = inputMenager.Movement * Time.deltaTime * 5;
-        transform.Translate(movieDirection, 0, 0);
+        float movieDirection = GameMenager.Instance.inputMenager.Movement;
+        transform.Translate(movieDirection * Time.deltaTime * movieSpead, 0, 0);
     }
 
 }
