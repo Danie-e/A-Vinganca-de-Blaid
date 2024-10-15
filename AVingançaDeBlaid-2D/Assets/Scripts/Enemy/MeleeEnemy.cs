cce5734b1c9041909d8f78ad8796b0ddad95b550
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-10-15 16:30:02 -0300

    Adicionando vida ao personagem e configurando dano de ataque.

--- AVingançaDeBlaid-2D/Assets/Scripts/Enemy/MeleeEnemy.cs
+++ AVingançaDeBlaid-2D/Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -8,29 +8,52 @@ public class MeleeEnemy : BaseEnemy
     [SerializeField] private Transform detectPosition;
     [SerializeField] private Vector2 detectBoxSize;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private float attackCooldown;
+
+    private float cooldownTimer;
 
     protected override void Update()
     {
+        cooldownTimer += Time.deltaTime;
         VerifyCanAttack();
     }
 
     private void VerifyCanAttack()
     {
+        if (cooldownTimer < attackCooldown)
+            return;
         if (PlayerInSight())
         {
             animator.SetTrigger("attack");
+            AttackPlayer();
         }
     }
 
     private bool PlayerInSight()
     {
-        Collider2D playerCollider = Physics2D.OverlapBox(detectPosition.position, detectBoxSize, 0f, playerLayer);
+        Collider2D playerCollider = CheckPlayerInDetectArea();
         return playerCollider != null;
     }
+
     private void OnDrawGizmos()
     {
         if (detectPosition == null) return;
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(detectPosition.position, detectBoxSize);
+    }
+
+    private void AttackPlayer()
+    {
+        if (CheckPlayerInDetectArea().TryGetComponent(out Health playerHealth))
+        {
+            print("Making player take damage");
+            playerHealth.TakeDamage();
+            cooldownTimer = 0;
+        }
+    }
+
+    private Collider2D CheckPlayerInDetectArea()
+    {
+        return Physics2D.OverlapBox(detectPosition.position, detectBoxSize, 0f, playerLayer);
     }
 }
