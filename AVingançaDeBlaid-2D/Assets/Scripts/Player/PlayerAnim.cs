b3d090cb32a79c231e0201d1eeac1ad4b07c2e69
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-10-15 16:41:47 -0300

    Ajustando animação do personagem levar dano.

--- AVingançaDeBlaid-2D/Assets/Scripts/Player/PlayerAnim.cs
+++ AVingançaDeBlaid-2D/Assets/Scripts/Player/PlayerAnim.cs
@@ -1,14 +1,19 @@
+using System;
 using UnityEngine;
 
 public class PlayerAnim : MonoBehaviour
 {
     private IsGroundedChecker isGroundedChecker;
     private Animator animator;
+    private Health playerHealth;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         isGroundedChecker = GetComponent<IsGroundedChecker>();
+        playerHealth = GetComponent<Health>();
+
+        playerHealth.OnHurt += PlayerHurtAnim;
     }
 
     private void Update()
@@ -17,5 +22,10 @@ public class PlayerAnim : MonoBehaviour
         animator.SetBool("isMoving", isMoving);
 
         animator.SetBool("isJumped", !isGroundedChecker.IsGrounded());
+    }
+
+    private void PlayerHurtAnim()
+    {
+        animator.SetTrigger("hurt");
     }
 }
