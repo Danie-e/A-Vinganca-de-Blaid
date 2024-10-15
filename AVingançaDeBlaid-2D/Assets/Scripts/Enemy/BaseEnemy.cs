45b3b231e90fa7b250756b01cdf9bd924ac18a14
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-10-15 19:02:13 -0300

    Adicionando animação de dano e morte do inimigo.

--- AVingançaDeBlaid-2D/Assets/Scripts/Enemy/BaseEnemy.cs
+++ AVingançaDeBlaid-2D/Assets/Scripts/Enemy/BaseEnemy.cs
@@ -1,12 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public abstract class BaseEnemy : MonoBehaviour
 {
     protected Animator animator;
+    protected Health health;
 
     protected virtual void Awake()
     {
         animator = GetComponent<Animator>();
+        health = GetComponent<Health>();
+
+        health.OnHurt += PlayHurtAnim;
+        health.OnDead += HandleDeath;
     }
     protected abstract void Update();
+
+    private void PlayHurtAnim() => animator.SetTrigger("hurt");
+
+    private void HandleDeath()
+    {
+        animator.SetTrigger("dead");
+        StartCoroutine(DestroyEnemy(2));
+    }
+
+    private IEnumerator DestroyEnemy(int time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(this.gameObject);
+
+    }
 }
