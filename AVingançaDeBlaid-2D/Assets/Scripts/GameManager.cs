844d01eea8cf01ac11162ce72e44068bd790b132
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-11-06 17:08:33 -0300

    Adicionando itens coletáveis a cena e criando zona para o boss onde será aberta se todas as chaves forem coletadas.

--- AVingançaDeBlaid-2D/Assets/Scripts/GameManager.cs
+++ AVingançaDeBlaid-2D/Assets/Scripts/GameManager.cs
@@ -1,19 +1,43 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public InputManager InputManager { get; private set; }
+    [Header("Dynamic Game Object")]
+    [SerializeField] private GameObject BossDoor;
 
     [Header("Managers")]
     public UIManager UIManager;
     public AudioManager AudioManager;
+
+    private int totalKeys;
+    private int keysLeftToCollect;
 
     private void Awake()
     {
         if (Instance != null) Destroy(this.gameObject);
         Instance = this;
 
+        totalKeys = FindObjectsOfType<CollectableKey>().Length;
+        keysLeftToCollect = totalKeys;
+        print(totalKeys);
+
         InputManager = new InputManager();
+    }
+
+    public void UpdateKeysLeft()
+    {
+        keysLeftToCollect--;
+        CheckAllKeysCollected();
+    }
+
+    private void CheckAllKeysCollected()
+    {
+        if (keysLeftToCollect <= 0)
+        {
+            Destroy(BossDoor);
+        }
     }
 }
